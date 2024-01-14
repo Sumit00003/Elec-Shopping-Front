37f502316f7c0f06a85c21348badd74ec53c14af
SumitM <95011832+Sumit00003@users.noreply.github.com>
2024-01-14 14:31:47 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,16 +39,16 @@ const Footer = () => {
                 <a href="tel: +91 9822011245" className='mt-3 d-block mb-2 text-white'>+91 9822011245</a>
                 <a href="mailto:sumitmouryaaa@gmail.com" className='mt-2 d-block mb-2 text-white'>sumitmouryaaa@gmail.com</a>
                 <div className='social_icons d-flex align-items-center gap-30 mt--4'>
-                  <a className='text-white' href=''>
+                  <a className='text-white' href='#'>
                     <BsLinkedin className='fs-4'/>
                   </a>
-                  <a className='text-white' href=''>
+                  <a className='text-white' href='#'>
                     <BsInstagram className='fs-4'/>
                   </a>
-                  <a className='text-white' href=''>
+                  <a className='text-white' href='#'>
                     <BsGithub className='fs-4'/>
                   </a>
-                  <a className='text-white' href=''>
+                  <a className='text-white' href='#'>
                     <BsYoutube className='fs-4'/>
                   </a>
                 </div>
